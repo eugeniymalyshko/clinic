101bdb2aa5154a81529a6157f05776f412344e6e
Yevhenii <eugeniymalyshko@gmail.com>
2025-09-19 12:56:17 +0300

    menu updated

--- source/js/files/script.js
+++ source/js/files/script.js
@@ -1,8 +1,10 @@
 // Menu burger
 const menuIcon = document.querySelector(".menu__icon");
 const body = document.querySelector("body");
+const menuBody = document.querySelector(".menu__body");
 
 menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle("_active");
+  menuBody.classList.toggle("_active");
   body.classList.toggle("_lock");
 });
