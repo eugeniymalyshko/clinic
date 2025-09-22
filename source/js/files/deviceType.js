81e64746c59edc6337b3a5517d438abe8ae16a07
Yevhenii <eugeniymalyshko@gmail.com>
2025-09-22 21:58:14 +0300

    ready header and hero

--- source/js/files/deviceType.js
+++ source/js/files/deviceType.js
@@ -1,27 +1,24 @@
 //---------- Device type-------------------
-let userAgent = navigator.userAgent.toLowerCase(),
-  width = screen.availWidth,
-  height = screen.availHeight,
-  userIsOnMobileDevice = checkIfUserIsOnMobileDevice(userAgent);
 
-if (userIsOnMobileDevice) {
+function isMobile() {
+  const regex =
+    /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  return regex.test(navigator.userAgent);
+}
+
+if (isMobile()) {
   document.body.classList.add("_touch");
+  //   const menusArrows = document.querySelectorAll(".menu__arrow");
+  const menusHub = document.querySelectorAll(".menu__link_hub");
+
+  if (menusHub.length > 0) {
+    for (let index = 0; index < menusHub.length; index++) {
+      const menuHub = menusHub[index];
+      menuHub.addEventListener("click", function (e) {
+        menuHub.parentElement.classList.toggle("_active");
+      });
+    }
+  }
 } else {
   document.body.classList.add("_pc");
-}
-function checkIfUserIsOnMobileDevice($userAgent) {
-  if ($userAgent.includes("mobi") || $userAgent.includes("tablet")) {
-    return true;
-  }
-  if ($userAgent.includes("android")) {
-    if (height > width && width < 800) {
-      // Screen is higher than it’s wide, so we have portrait mode
-      return true;
-    }
-    if (width > height && height < 800) {
-      // Screen is wider than it’s high, so we have landscape mode
-      return true;
-    }
-  }
-  return false;
 }
